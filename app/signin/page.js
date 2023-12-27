34347442530a4ba9383231fd3969907b853d4825
김한들 <dle1883@gmail.com>
2023-12-27 16:36:59 +0900

    login page 꾸미기

--- app/signin/page.js
+++ app/signin/page.js
@@ -1,32 +1,52 @@
 'use client'
 import React, { signIn } from "next-auth/react"
-import { useRef } from "react"
+// import { useRef } from "react"
 export default function Login() {
 
-    const emailRef = useRef(null)
-    const passwordRef = useRef(null)
+    // const emailRef = useRef(null)
+    // const passwordRef = useRef(null)
 
 
-    const handLeSubmit = async () => {
-        const result = await signIn("credentials", {
-            username: emailRef.current,
-            password: passwordRef.current,
-            redirect: true,
-            callbackUrl: "/",
-        });
+    // const handLeSubmit = async () => {
+    //     const result = await signIn("credentials", {
+    //         username: emailRef.current,
+    //         password: passwordRef.current,
+    //         // redirect: true,
+    //         redirect: {destination: "/"},
+    //         callbackUrl: "/",
+    //     });
+    // }
 
-    }
+    // const handleSignIn = async (provider) => {
+    //     await signIn(provider);
+    // };
+
     const handleSignIn = async (provider) => {
-        await signIn(provider);
+        await signIn(provider, { callbackUrl: '/' })
     };
 
     return (
-        <div>
-            <button onClick={() => { handleSignIn('github') }}>깃허브 로그인</button>
-            <button onClick={() => { handleSignIn('github') }}>카카오 로그인</button>
-            <button onClick={() => { handleSignIn('github') }}>네이버 로그인</button>
+        <div className="layout-lg">
+            <div className="layout-inner">
+                <div className="login-wrapper">
+                    <h1>간편로그인 후<br></br>이용이<br></br>가능합니다.</h1>
+                    <div className="login-wrap">
+                        <button className="github" onClick={() => { handleSignIn('github') }}>
+                            <img src="./github.png" alt="" />
+                            <span>깃허브 로그인</span>
+                        </button>
+                        <button className="kakao" onClick={() => { handleSignIn('kakao') }}>
+                            <img src="./kakao.png" alt="" />
+                            <span>카카오 로그인</span>
+                        </button>
+                        <button className="naver" onClick={() => { handleSignIn('naver') }}>
+                            <img src="./naver.png" alt="" />
+                            <span>네이버 로그인</span>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
         </div>
-
     )
-
 }
