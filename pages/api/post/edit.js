9ddbf96c0530eb1cc3f7e45dae7746bb465ba3de
김한들 <dle1883@gmail.com>
2024-01-24 09:20:12 +0900

    댓글 삭제기능 구현 완료

--- pages/api/post/edit.js
+++ pages/api/post/edit.js
@@ -10,30 +10,11 @@ export default async function handler(req, res) {
     // const result = await db.collection('post').findOne({ _id: new ObjectId(props.params.id) });
     // let session = await getServerSession(authOptions)
     // console.log(session);
+    
     let changed = {
         title: req.body.title,
         content:req.body.content
     }
-
-    // let result = await db.collection('post').updateOne(
-    //     {_id: },
-    //     {$set : req.body }
-    // )
-    
-    
-    // if (req.method === "POST") {
-
-    //     if (req.body.title == '') {
-    //         return res.status(500).json('빈칸을 채워주세요.')
-    //     }
-
-    //     try {
-    //         await db.collection('post').insertOne(req.body)
-    //         res.redirect(302, '/list')
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // }
 
     if (req.method === "POST") {
         if (req.body.title == '') {
@@ -68,20 +49,3 @@ export default async function handler(req, res) {
     //     }
     // }
 }
-// let client = await connectDB;
-// const db = client.db("next");
-// // let result = await db.collection('post').find().toArray();
-
-// if (req.method == 'POST') {
-//     if (req.body.title == '') {
-//         return res.status(500).json('제목써라')
-//     }
-//     try {
-//         let db = (await connectDB).db('next')
-//         let result = db.collection('post').insertOne(res.body)
-//         res.redirect(302, '/list')
-//     } catch (error) {
-//         console.log(error)
-//     }
-
-// }
